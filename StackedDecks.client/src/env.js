7181d2231a3ac5c52491f9fde02cc953feb8f031
Jacob <Jacobzeme8@gmail.com>
2023-03-08 13:13:12 -0700

    evn set up

--- StackedDecks.client/src/env.js
+++ StackedDecks.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'jacobzeme8.us.auth0.com'
+export const clientId = 'K799U2HHVsa9CjMS7abRMinjFBb3IPV9'
+export const audience = 'https://codeworks-yakub-api.com'
