d5e9d3262edfbe31d649e23746721b87919a5267
Jacob <Jacobzeme8@gmail.com>
2023-03-08 13:56:51 -0700

    db condext card in

--- StackedDecks/server/db/DbContext.js
+++ StackedDecks/server/db/DbContext.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
+import { CardSchema } from "../models/Card";
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+
+  Cards = mongoose.model('Card', CardSchema);
 }
 
 export const dbContext = new DbContext()
