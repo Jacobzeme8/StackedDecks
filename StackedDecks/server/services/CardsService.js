2e914b04ebddbb2971f7e98954138ddda4a05b50
Jacob <Jacobzeme8@gmail.com>
2023-03-08 14:51:54 -0700

    get request for all cards

--- StackedDecks/server/services/CardsService.js
+++ StackedDecks/server/services/CardsService.js
@@ -1,6 +1,10 @@
 import { dbContext } from "../db/DbContext"
 
 class CardsService{
+  async getAllCards() {
+    const cards = await dbContext.Cards.find()
+    return cards
+  }
   async createCard(cardData) {
     const card = await dbContext.Cards.create(cardData)
     return card
