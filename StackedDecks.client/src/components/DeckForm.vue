b438324a6a8a5be322e1ee613859708729f3e28d
Jacob <Jacobzeme8@gmail.com>
2023-03-09 12:28:01 -0700

    form dropdown done

--- StackedDecks.client/src/components/DeckForm.vue
+++ StackedDecks.client/src/components/DeckForm.vue
@@ -17,15 +17,70 @@
                     id="description" aria-describedby="emailHelp">
             </div>
             <label for="type" class="form-label">Deck Type:</label>
-            <select v-model="editable.type" class="form-select mb-3" aria-label="Default select example" id="type">
-                <option selected value="arms">Arms</option>
-                <option value="back">Back</option>
-                <option value="cardio">Cardio</option>
-                <option value="chest">Chest</option>
-                <option value="core">Core</option>
-                <option value="legs">Legs</option>
-                <option value="shoulders">Shoulders</option>
-            </select>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Dropdown button
+                </button>
+                <ul class="dropdown-menu dropdown-menu-dark">
+                    <li>
+                        <div class="form-check ms-1">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                arms
+                            </label>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="form-check ms-1">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                legs
+                            </label>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="form-check ms-1">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                back
+                            </label>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="form-check ms-1">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                chest
+                            </label>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="form-check ms-1">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                cardio
+                            </label>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="form-check ms-1">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                core
+                            </label>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="form-check ms-1">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                shoulders
+                            </label>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
         <div class="modal-footer">
             <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
