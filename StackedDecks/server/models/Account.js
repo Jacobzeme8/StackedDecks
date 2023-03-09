b23b967f67b9d7e6e6f550bfe4291e5ff9cfe7ed
Jacob <Jacobzeme8@gmail.com>
2023-03-09 16:53:58 -0700

    defautl cover image serever side

--- StackedDecks/server/models/Account.js
+++ StackedDecks/server/models/Account.js
@@ -7,7 +7,7 @@ export const AccountSchema = new Schema(
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
     picture: { type: String, required: true },
-    coverImg: { type: String, required: true },
+    coverImg: { type: String, required: true, default: "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" },
     level: { type: String, enum: ['beginner', 'intermediate', 'expert'], default: 'beginner' },
     bio: { type: String }
     // NOTE If you wish to add additional properties do so here
