c195fd7bd3a757dd588db39bac87a39083cd0e43
Linda Taing <lindatdoan@me.com>
2023-03-09 06:58:38 -0700

    adjusted Cards MaxLength on instructions

--- StackedDecks/server/models/Card.js
+++ StackedDecks/server/models/Card.js
@@ -5,10 +5,10 @@ const Schema = mongoose.Schema
 export const CardSchema = new Schema({
   name: { type: String, required: true },
   muscleGroup: { type: String, required: true, enum: ['arms', 'back', 'chest', 'core', 'legs', 'shoulders', 'cardio'] },
-  instruction: { type: String, required: true, minlength: 10, maxlength: 400 },
+  instruction: { type: String, required: true, minlength: 10, maxLength: 1000 },
   difficulty: { type: String, required: true, enum: ['beginner', 'intermediate', 'expert'] },
-  imgUrl: { type: String, required: true, maxlength: 500 },
-  equipment: { type: String, maxlength: 50 }
+  imgUrl: { type: String, required: true, maxLength: 500 },
+  equipment: { type: String, maxLength: 50 }
 },
   { timestamps: true, toJSON: { virtuals: true } }
 )
