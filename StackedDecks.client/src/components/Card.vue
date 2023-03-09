bebaa5c946ec190d8f0259a71ee1f4e189ca8af5
Parker <ward.parker26@gmail.com>
2023-03-09 11:38:06 -0700

    flip card half working

--- StackedDecks.client/src/components/Card.vue
+++ StackedDecks.client/src/components/Card.vue
@@ -1,26 +1,91 @@
 <template>
-  <div class="Card card my-3">
-    <img :src="card.imgUrl" :alt="card.name">
-  </div>
-  <div class="card-body text-center">
-    <p><b>{{ card.name }}</b></p>
+  <!-- NOTE Face of Card -->
+  <div class="Card  my-3">
+    <div class="kard">
+      <div class="kard_face kard-front">
+        <img class="img-fluid" :src="card.imgUrl" :alt="card.name">
+        <div class="card-body text-center">
+          <p><b>{{ card.name }}</b></p>
+          <p>CLICK IMAGE TO SEE DETAILS</p>
+        </div>
+      </div>
+
+      <!-- NOTE Back of card -->
+      <div class="kard_face kard-back">
+        <img class="img-fluid" :src="card.imgUrl" :alt="card.name">
+        <div class="Deck_Card_Info">
+          <p>{{ card.muscleGroup }}</p>
+          <p>Back Side</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script>
 import { Card } from '../models/Card.js';
+// import { DeckCard } from '../models/DeckCard.js';
 
 export default {
   props: {
-    card: { type: Card, required: true }
+    card: { type: Card, required: true },
+    // deckCard: { type: DeckCard, required: true }
   },
   setup() {
 
-    return {}
+    var kards = document.querySelectorAll('.kard');
+    [...kards].forEach((kard) => {
+      kard.addEventListener('click', function () {
+        kard.classList.toggle('is-flipped');
+      });
+    });
+
+
+
+    // let kard = document.querySelectorAll('.kard')
+    // [kard].forEach((kard) => {flipCard(){
+    //   kard.classList.toggle('is-flipped');
+    // }
+    // })
+    return {
+
+
+    }
+
   }
 }
+
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.kard_face {
+  // position: absolute;
+
+  backface-visibility: hidden;
+}
+
+.kard {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  transform-style: preserve-3d;
+  transform-origin: center right;
+  transition: transform 1s;
+
+}
+
+.kard.is-flipped {
+  transform: translateX(-100%) rotateY(-180deg);
+}
+
+
+
+.kard-back {
+  position: fixed;
+  top: 0%;
+  transform: rotateY(180deg)
+}
+</style>
