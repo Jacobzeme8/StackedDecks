e050d2660ead2fae00b5f28024bb9084ae2e4260
Jacob <Jacobzeme8@gmail.com>
2023-03-10 16:40:46 -0700

    app.vue not done

--- StackedDecks.client/src/App.vue
+++ StackedDecks.client/src/App.vue
@@ -3,7 +3,23 @@
     <Navbar />
   </header>
   <main class="bg-color">
-    <router-view />
+    <div class="container-fluid">
+      <div class="row ">
+        <div class="col-2 d-flex flex-column">
+          <button @click="changeFilterType('all')" class="btn btn-outline-light text-dark">All</button>
+          <button @click="changeFilterType('arms')" class="btn btn-outline-light text-dark">Arms</button>
+          <button @click="changeFilterType('back')" class="btn btn-outline-light text-dark">Back</button>
+          <button @click="changeFilterType('cardio')" class="btn btn-outline-light text-dark">Cardio</button>
+          <button @click="changeFilterType('chest')" class="btn btn-outline-light text-dark">Chest</button>
+          <button @click="changeFilterType('core')" class="btn btn-outline-light text-dark">Core</button>
+          <button @click="changeFilterType('legs')" class="btn btn-outline-light text-dark">Legs</button>
+          <button @click="changeFilterType('shoulders')" class="btn btn-outline-light text-dark">Shoulders</button>
+        </div>
+        <div class="col-10">
+          <router-view />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
