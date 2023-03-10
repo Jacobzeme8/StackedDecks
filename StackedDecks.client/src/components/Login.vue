d1d8c0a38e801ea698977b0753d0781afebe6cf5
Linda Taing <lindatdoan@me.com>
2023-03-10 16:21:26 -0700

    login bg gone

--- StackedDecks.client/src/components/Login.vue
+++ StackedDecks.client/src/components/Login.vue
@@ -6,9 +6,9 @@
     </button>
     <div v-else>
       <div class="dropdown dropstart my-2 my-lg-0">
-        <div type="button" class="bg-dark border-0 selectable no-select" data-bs-toggle="dropdown" aria-expanded="false">
+        <div type="button" class="border-0 selectable no-select" data-bs-toggle="dropdown" aria-expanded="false">
           <div v-if="account.picture || user.picture">
-            <img :src="account.picture || user.picture" alt="account photo" height="50" width="50"
+            <img :src="account.picture || user.picture" alt="account photo" height="60" width="60"
               class="rounded-circle" />
           </div>
         </div>
