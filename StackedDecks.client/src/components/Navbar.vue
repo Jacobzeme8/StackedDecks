7a81251c5ac54440a1d491bccb1591db847c94ab
Linda Taing <lindatdoan@me.com>
2023-03-09 20:23:55 -0700

    removed green line

--- StackedDecks.client/src/components/Navbar.vue
+++ StackedDecks.client/src/components/Navbar.vue
@@ -62,7 +62,7 @@ a:hover {
 }
 
 .navbar-nav .router-link-exact-active {
-  border-bottom: 2px solid var(--bs-success);
+  /* border-bottom: 2px solid var(--bs-success); */
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
 }
