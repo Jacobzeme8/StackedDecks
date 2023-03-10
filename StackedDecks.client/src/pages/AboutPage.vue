c37553acf8c7766c5aec0f9f56fdb94f634b4202
Linda Taing <lindatdoan@me.com>
2023-03-09 22:12:20 -0700

    Adjusted About Page and added other font types.

--- StackedDecks.client/src/pages/AboutPage.vue
+++ StackedDecks.client/src/pages/AboutPage.vue
@@ -1,52 +1,60 @@
 <template>
-  <h1>This is the about page</h1>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-6">
-        <div class="">
+  <div class="container-fluid bg-pic">
+    <h1 class="text-center text-dark letter">About our Devs</h1>
+
+    <div class="container-fluid">
+      <div class="row d-flex justify-content-around">
+        <div class="col-md-4">
           <div class="img-fluid">
-            <img
+            <img class="photo"
               src="https://images.unsplash.com/photo-1605296867304-46d5465a13f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fHdvcmtvdXR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
               alt="Parker">
           </div>
-          <div class="">
-            <span><b class="text-dark">Parker Ward</b></span>
-            <p><b> 6 Ft. & Stacked</b></p>
+          <div class="text-center">
+            <span><b class="letter text-danger">Parker Ward</b></span>
+            <p class="letter text-danger"><b> 6 Ft. & Stacked</b></p>
           </div>
         </div>
-        <div>
+        <div class="col-md-4">
           <div class="img-fluid">
-            <img
+            <img class="photo"
               src="https://s3-alpha-sig.figma.com/img/ca32/e76f/a7d4396b4509a88d57a61fedfea6fb02?Expires=1679270400&Signature=LduP3mFlvgDR9qfA6iDXXtemmIqhSKr0b8Bug9-DhiKUkFgDd5nTnmhVGWogIGhZymli6R4Z06lnoMHo0dSQ3nPi~RvjmEuMbnQF1x5zDctvSMHTVtLvj~BZYfQ5~b3u0ssyQhNr074NF-8HpQLT2BsLY2DXRhiNiUSISk7sWgYsw3fp-OSmBvfGCUm6~EK0qikRmJ2R4jvXb50HqfoEAqnpVdRQIBY2XDeEid2NAXr~Gr4vE6CGNr4YxEE2m9jAEw86I8ejBfsUkGseiKN0Qz4-k-fb-pLUUFKhzf6e2FBu4QMhO2dTJ5d3X2bkOEIWlpRltngnQFOTYZGkzidEHw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
               alt="T-Rex">
           </div>
-          <div>
-            <span><b>Thomas Fuhriam</b></span>
-            <p><b> Wannabe Stacked
+          <div class="text-center">
+            <span><b class="letter text-danger">Thomas Fuhriam</b></span>
+            <p class="letter text-danger"><b>Wannabe Stacked
                 Strong Man and amateur arm wrestler</b></p>
           </div>
         </div>
       </div>
-      <div class="col-md-6">
-        <div>
-          <img
-            src="https://s3-alpha-sig.figma.com/img/22f1/52fd/fac66f32e080a8dab57432a507885bdb?Expires=1679270400&Signature=ItlnLXb3EGbqossIWSdzWCg5kJDPLppZ5Q6hAE7p5F2S20hxZpuoToYysuXoOSAc4bKAahCvHhiYrQV393IFWi4-Jp9Q99YukzZIt37PIm5AjXAu8~RufcfYZoyMZC~jEfAAqPHhEBUQSiSupZF7tRTWe6iEHqyQxeT817dmUy0LZbGAHoljqTMA~~YCipTvYukaVZPsLuujGUB-HxrVNeLKVh4LqatIk7Ly2nd5te-tB81XEXmQyVd-GKJZ74a96GBvrjaQNAmcgfp30Db6XQXpmuDCbYyTsNt~vqR-03oWknylooCKZwAE5EqVg9pna9QLn7drDYTegRXNe2aepw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-            alt="Linda Taing">
+    </div>
+
+    <div class="container-fluid">
+      <div class="row d-flex justify-content-around">
+        <div class="col-md-4">
+          <div class="img-fluid">
+            <img class="photo"
+              src="https://s3-alpha-sig.figma.com/img/22f1/52fd/fac66f32e080a8dab57432a507885bdb?Expires=1679270400&Signature=ItlnLXb3EGbqossIWSdzWCg5kJDPLppZ5Q6hAE7p5F2S20hxZpuoToYysuXoOSAc4bKAahCvHhiYrQV393IFWi4-Jp9Q99YukzZIt37PIm5AjXAu8~RufcfYZoyMZC~jEfAAqPHhEBUQSiSupZF7tRTWe6iEHqyQxeT817dmUy0LZbGAHoljqTMA~~YCipTvYukaVZPsLuujGUB-HxrVNeLKVh4LqatIk7Ly2nd5te-tB81XEXmQyVd-GKJZ74a96GBvrjaQNAmcgfp30Db6XQXpmuDCbYyTsNt~vqR-03oWknylooCKZwAE5EqVg9pna9QLn7drDYTegRXNe2aepw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt="Linda Taing">
+          </div>
+          <div class="text-center">
+            <span><b class="letter text-danger">Linda Taing</b></span>
+            <p class="letter text-danger"><b>Cyclist that goes nowhere, but stacked!</b></p>
+          </div>
         </div>
-        <div>
-          <span><b>Linda Taing</b></span>
-          <p><b> Cyclist that goes nowhere, but stacked!</b></p>
+        <div class="col-md-4">
+          <div class="img-fluid">
+            <img class="photo"
+              src="https://s3-alpha-sig.figma.com/img/554a/a6ca/8e19626ad6bf77da2fc244731d8ae92a?Expires=1679270400&Signature=T2t8SadoDphDXYw9TbVfPOhXDVzYWjX9SPufsQFb3xIIFdCE~sxscahGCuJ4RQyRsH6SeYR66UAgjSOgh9GRhkVNaFIHFTeYwWl5PT3QfythJrg9X4BC0LrgsScj2J87BVi4LH7KyQ1TvNsPpacKRTVwFIIWRPtllliioJV~Ox2yiiwHbdA3TMlsDKl8miCkUxHAi~bb2b4QyyeVyUxOkRiaNjVdlIYwLBTlRwabPoFnr1oXdiElKxtc1~Q~a~EONQPLAhVFIIu-nlAH1LjY10JiSgCuyi15H9qfbzqM~FBN1XWRRHhhyXf-Aczawpdz~0sDzGOfmY3p5KCKSPTf7A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt="JACOB MARTINEZ">
+          </div>
+          <div class="text-center">
+            <span><b class="letter text-danger ">Jacob Martinez</b></span>
+            <p class="letter text-danger"><b>5’11 and 3/4
+                in the morning, STACKED</b></p>
+          </div>
         </div>
-      </div>
-      <div>
-        <img
-          src="https://s3-alpha-sig.figma.com/img/554a/a6ca/8e19626ad6bf77da2fc244731d8ae92a?Expires=1679270400&Signature=T2t8SadoDphDXYw9TbVfPOhXDVzYWjX9SPufsQFb3xIIFdCE~sxscahGCuJ4RQyRsH6SeYR66UAgjSOgh9GRhkVNaFIHFTeYwWl5PT3QfythJrg9X4BC0LrgsScj2J87BVi4LH7KyQ1TvNsPpacKRTVwFIIWRPtllliioJV~Ox2yiiwHbdA3TMlsDKl8miCkUxHAi~bb2b4QyyeVyUxOkRiaNjVdlIYwLBTlRwabPoFnr1oXdiElKxtc1~Q~a~EONQPLAhVFIIu-nlAH1LjY10JiSgCuyi15H9qfbzqM~FBN1XWRRHhhyXf-Aczawpdz~0sDzGOfmY3p5KCKSPTf7A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-          alt="JACOB MARTINEZ">
-      </div>
-      <div>
-        <span><b>JACOB MARTINEZ</b></span>
-        <p><b> 5’11 AND 3/4
-            in the morning, STACKED</b></p>
       </div>
     </div>
   </div>
@@ -61,3 +69,27 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+.photo {
+  height: 25em;
+  width: 25em;
+  object-fit: cover;
+  border-radius: 2%;
+}
+
+
+
+.bg-pic {
+  background-image: url(https://images.unsplash.com/photo-1548705085-101177834f47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTUyfHx3b3Jrb3V0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60);
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
+  opacity: 80;
+}
+
+.letter {
+  font-family: 'M PLUS 1p', sans-serif;
+  text-shadow: 1px 1px 1px black;
+}
+</style>
