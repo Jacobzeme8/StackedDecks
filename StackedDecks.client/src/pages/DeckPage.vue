a006b2f89d6d07e3666d1a6d56e084caa10f8f8e
Linda Taing <lindatdoan@me.com>
2023-03-10 14:32:09 -0700

    DeckPage Styling almost done

--- StackedDecks.client/src/pages/DeckPage.vue
+++ StackedDecks.client/src/pages/DeckPage.vue
@@ -1,9 +1,45 @@
 <template>
   <div v-if="deck">
-    <h1>{{ deck.name }}</h1>
+    <div class="container-fluid">
+      <div class="row d-flex bg-banner">
+        <div class="col-md-2"></div>
+        <div class="col-md-8 p-2">
+          <div class="card elevation-5 mx-auto frosted">
+            <div class="card-body">
+              <h6 title="Stacked progress is the mastery of this deck." class="text-center">Stacked Progress</h6>
+              <div class="d-flex justify-content-center">
+                <div class="progress w-50 " role="progressbar" aria-label="Example 20px high" aria-valuenow="25"
+                  aria-valuemin="0" aria-valuemax="100" style="height: 20px">
+                  <div class="progress-bar" style="width: 25%"></div>
+                </div>
+              </div>
+              <div class="d-flex justify-content-center pt-2">
+                <h1 class="text-center">My {{ deck.name }} Deck </h1>
+                <img class="img-fluid ms-3 rounded-circle profile-img" :src="deck.creator.picture" alt="">
+              </div>
+              <h6 class="text-center">Current Exercise Progress</h6>
 
+              <div class="d-flex justify-content-center">
+                <div class="progress w-25 " role="progressbar" aria-label="Example 1px high" aria-valuenow="25"
+                  aria-valuemin="0" aria-valuemax="100" style="height: 1px">
+                  <div class="progress-bar" style="width: 25%"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+        </div>
+      </div>
+    </div>
     <div v-for="d in deckCards">
-      <Card :card="d.card" />
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <Card :card="d.card" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -66,7 +102,9 @@ export default {
     return {
       route,
       deck: computed(() => AppState.deck),
-      deckCards: computed(() => AppState.deckCards)
+      deckCards: computed(() => AppState.deckCards),
+      coverImg: computed(() => `url("${AppState.deck?.coverImg}")`)
+
       // card: computed(() => AppState.deckCard.card)
 
     };
@@ -76,4 +114,35 @@ export default {
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.bg-banner {
+
+  background-image: v-bind(coverImg);
+  background-size: cover;
+  background-position: center;
+  opacity: 80%;
+  height: 40vh;
+}
+
+.width-card {
+  width: 40vh;
+}
+
+.profile-img {
+  height: 45px;
+  width: 45px;
+}
+
+.frosted {
+  box-shadow: 0 0 5px 0;
+  background: inherit;
+  backdrop-filter: blur(80px);
+  /* margin: 100px; */
+}
+
+.frosted-x {
+  box-shadow: 0 0 5px 0;
+  background: inherit;
+  /* margin: 100px; */
+}
+</style>
