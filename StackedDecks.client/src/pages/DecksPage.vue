ca5b54818525c3e3d97383f0a8b84e003a53bcff
Parker <ward.parker26@gmail.com>
2023-03-14 09:51:27 -0600

    decks page update

--- StackedDecks.client/src/pages/DecksPage.vue
+++ StackedDecks.client/src/pages/DecksPage.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="container-fluid bg-pic rounded-top">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="text-light">Posted Decks:</h1>
+      </div>
+    </div>
     <div class="row" v-if="decks">
       <div v-for="deck in decks" class="col-md-3">
         <Deck :deck="deck" />
