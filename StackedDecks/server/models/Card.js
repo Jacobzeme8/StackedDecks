963c388cc2da21a41dca87fc1655786ba213c510
Linda Taing <lindatdoan@me.com>
2023-03-14 09:28:57 -0600

    added to card schema

--- StackedDecks/server/models/Card.js
+++ StackedDecks/server/models/Card.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 export const CardSchema = new Schema({
   name: { type: String, required: true },
-  muscleGroup: { type: String, required: true, enum: ['arms', 'back', 'chest', 'core', 'legs', 'shoulders', 'cardio', 'full body'] },
+  muscleGroup: { type: String, required: true, enum: ['arms', 'back', 'chest', 'core', 'legs', 'shoulders', 'cardio', 'full body', 'upper body', 'lower body'] },
   instruction: { type: String, required: true, minlength: 10, maxLength: 2000 },
   difficulty: { type: String, required: true, enum: ['beginner', 'intermediate', 'expert'] },
   imgUrl: { type: String, required: true, maxLength: 500 },
