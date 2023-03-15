937e54915b20cbdb42a077a20455696652150ac1
ThomF <seniorswanky@icloud.com>
2023-03-15 15:44:28 -0600

    fun button

--- StackedDecks.client/src/App.vue
+++ StackedDecks.client/src/App.vue
@@ -80,7 +80,7 @@
           </div>
           <div class="mb-2">
             <router-link :to="{ name: 'About' }" class="d-flex flex-column mb-2">
-              <button class="btn btn-outline-light filter-buttons bebas flex-fill">About</button>
+              <span><a></a></span>
             </router-link>
           </div>
         </div>
@@ -224,5 +224,54 @@ footer {
   opacity: .65;
   text-transform: uppercase;
   text-shadow: none;
+}
+
+// NOTE Fun stuff
+span {
+  position: relative;
+  display: inline-flex;
+  width: 180px;
+  height: 55px;
+  margin: 0 15px;
+  perspective: 1000px;
+}
+
+span a {
+  font-size: 19px;
+  letter-spacing: 1px;
+  transform-style: preserve-3d;
+  transform: translateZ(-25px);
+  transition: transform .25s;
+  font-family: 'Bebas Neue', cursive;
+
+}
+
+span a:before,
+span a:after {
+  position: absolute;
+  content: "About Us!";
+  height: 55px;
+  width: 180px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 5px solid black;
+  box-sizing: border-box;
+  border-radius: 5px;
+}
+
+span a:before {
+  color: #fff;
+  background: rgba(160, 70, 123, 0.684);
+  transform: rotateY(0deg) translateZ(25px);
+}
+
+span a:after {
+  color: #000;
+  transform: rotateX(90deg) translateZ(25px);
+}
+
+span a:hover {
+  transform: translateZ(-25px) rotateX(-90deg);
 }
 </style>
