197ca12ad31e8e90549c07d4057c681e68c096f9
Parker <ward.parker26@gmail.com>
2023-03-15 16:17:47 -0600

    about page update for mobile

--- StackedDecks.client/src/pages/AboutPage.vue
+++ StackedDecks.client/src/pages/AboutPage.vue
@@ -6,7 +6,7 @@
       <div class="row d-flex justify-content-around">
         <div class="col-md-4">
           <div class="img-fluid">
-            <img class="photo"
+            <img class="photo img-fluid"
               src="https://s3-alpha-sig.figma.com/img/9791/bff8/42acbb7b196f8fb9f3f5be885726565b?Expires=1679270400&Signature=RXR5rQD5MdmLhke4ysu59B9A4wGFJXR4rVQd5MzR0J118u5zu26OXF0L76V0n5UcpSS3X4vAIgxNH3V4I~ly8H~5zJOmx-UPriYRPbYW6K1MHSEZXO9aVlRfMqs8GQrmNbqExR3rs~8S4Wf~mtqOeqpK4k1jwfYtx9ZICOfWvVev3m3-gbvLbDgdYDf13J5ldZIiSEQdT4OyLXV7PvW5czgwjcHvbXI9QTXGpfoQQL2NhtBwzCrU5vrDyoA6G2-jbJmZo89eiMqA6IcpaAzshpIwcopuMGW8IDehtWdf2z7acUYCy7ZGcq6JPheYPZjn~5MqCBgRu682Jeh8iW1SUg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
               alt="Parker">
           </div>
@@ -17,7 +17,7 @@
         </div>
         <div class="col-md-4">
           <div class="img-fluid">
-            <img class="photo"
+            <img class="photo img-fluid"
               src="https://s3-alpha-sig.figma.com/img/ca32/e76f/a7d4396b4509a88d57a61fedfea6fb02?Expires=1679270400&Signature=LduP3mFlvgDR9qfA6iDXXtemmIqhSKr0b8Bug9-DhiKUkFgDd5nTnmhVGWogIGhZymli6R4Z06lnoMHo0dSQ3nPi~RvjmEuMbnQF1x5zDctvSMHTVtLvj~BZYfQ5~b3u0ssyQhNr074NF-8HpQLT2BsLY2DXRhiNiUSISk7sWgYsw3fp-OSmBvfGCUm6~EK0qikRmJ2R4jvXb50HqfoEAqnpVdRQIBY2XDeEid2NAXr~Gr4vE6CGNr4YxEE2m9jAEw86I8ejBfsUkGseiKN0Qz4-k-fb-pLUUFKhzf6e2FBu4QMhO2dTJ5d3X2bkOEIWlpRltngnQFOTYZGkzidEHw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
               alt="T-Rex">
           </div>
@@ -34,7 +34,7 @@
       <div class="row d-flex justify-content-around">
         <div class="col-md-4">
           <div class="img-fluid">
-            <img class="photo"
+            <img class="photo img-fluid"
               src="https://s3-alpha-sig.figma.com/img/22f1/52fd/fac66f32e080a8dab57432a507885bdb?Expires=1679270400&Signature=ItlnLXb3EGbqossIWSdzWCg5kJDPLppZ5Q6hAE7p5F2S20hxZpuoToYysuXoOSAc4bKAahCvHhiYrQV393IFWi4-Jp9Q99YukzZIt37PIm5AjXAu8~RufcfYZoyMZC~jEfAAqPHhEBUQSiSupZF7tRTWe6iEHqyQxeT817dmUy0LZbGAHoljqTMA~~YCipTvYukaVZPsLuujGUB-HxrVNeLKVh4LqatIk7Ly2nd5te-tB81XEXmQyVd-GKJZ74a96GBvrjaQNAmcgfp30Db6XQXpmuDCbYyTsNt~vqR-03oWknylooCKZwAE5EqVg9pna9QLn7drDYTegRXNe2aepw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
               alt="Linda Taing">
           </div>
@@ -45,7 +45,7 @@
         </div>
         <div class="col-md-4">
           <div class="img-fluid">
-            <img class="photo"
+            <img class="photo img-fluid"
               src="https://s3-alpha-sig.figma.com/img/554a/a6ca/8e19626ad6bf77da2fc244731d8ae92a?Expires=1679270400&Signature=T2t8SadoDphDXYw9TbVfPOhXDVzYWjX9SPufsQFb3xIIFdCE~sxscahGCuJ4RQyRsH6SeYR66UAgjSOgh9GRhkVNaFIHFTeYwWl5PT3QfythJrg9X4BC0LrgsScj2J87BVi4LH7KyQ1TvNsPpacKRTVwFIIWRPtllliioJV~Ox2yiiwHbdA3TMlsDKl8miCkUxHAi~bb2b4QyyeVyUxOkRiaNjVdlIYwLBTlRwabPoFnr1oXdiElKxtc1~Q~a~EONQPLAhVFIIu-nlAH1LjY10JiSgCuyi15H9qfbzqM~FBN1XWRRHhhyXf-Aczawpdz~0sDzGOfmY3p5KCKSPTf7A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
               alt="JACOB MARTINEZ">
           </div>
