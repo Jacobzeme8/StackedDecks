2aa6cf6532d8a97faaf977dcbf8655070c293143
Jacob <Jacobzeme8@gmail.com>
2023-03-16 09:53:37 -0600

    exp in backend for decks and account

--- StackedDecks/server/models/Deck.js
+++ StackedDecks/server/models/Deck.js
@@ -7,6 +7,7 @@ export const DeckSchema = new Schema({
     description: { type: String, maxLength: 1000, required: true },
     coverImg: { type: String, maxLength: 500, default: "https://images.unsplash.com/photo-1584735935682-2f2b69dff9d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d29ya291dHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" },
     creatorId: { type: Schema.Types.ObjectId, required: true },
+    exp: { type: Number, default: 0 },
     isPublic: { type: Boolean, default: false, required: true },
     exerciseType: { type: [tag] },
     isCopied: { type: Boolean, default: false }
