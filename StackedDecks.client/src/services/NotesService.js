2ed0c2356d56a747812cac499dffdd17a60efcb4
Linda Taing <lindatdoan@me.com>
2023-03-16 15:46:24 -0600

    c

--- StackedDecks.client/src/services/NotesService.js
+++ StackedDecks.client/src/services/NotesService.js
@@ -1,8 +1,10 @@
 import { AppState } from "../AppState.js"
 import { api } from "./AxiosService.js"
+import { logger } from "../utils/Logger.js"
 
 class NotesService {
     async createNote(noteData, deckId) {
+        logger.log('You here Note?')
         const res = await api.post(`api/notes/${deckId}`, noteData)
         logger.log('creating note in service')
 
